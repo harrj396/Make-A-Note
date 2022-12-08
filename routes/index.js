ade71eee85562d1bd1007dbf15321534d7a6e87d
harrj396 <ahletmethink@gmail.com>
2022-12-08 03:41:18 -0500

    server currently sends to welcome page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const noteRouter = require('./notes')
 const app = express();
-app.use('./notes', noteRouter);
+app.use('/notes', noteRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const api = require('./routes/index.js');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
-// app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+app.use(express.static('public'));
 
 
 
